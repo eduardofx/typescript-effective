138c7fdb6d71818f7a755d32b6cd0c013303a05e
Eduardo Kawassaki <eduardok.fx@gmail.com>
2020-07-08 21:09:50 -0300

    feat: change name from file

---
+++ modules/StringValidatorInterface.ts
@@ -0,0 +1,3 @@
+export interface StringValidatorInterface {
+  isAcceptable(s: string): boolean;
+}
--- modules/ZipValidator.ts
+++ modules/ZipValidator.ts
@@ -1,8 +1,8 @@
-import { StringValidator } from "./StringValidator";
+import { StringValidatorInterface } from "./StringValidatorInterface";
 
 export const numberRegexp = /^[0-9]+$/;
 
-export class ZipCodeValidator implements StringValidator {
+export class ZipCodeValidator implements StringValidatorInterface {
   isAcceptable(s: string) {
     return s.length === 5 && numberRegexp.test(s);
   }
