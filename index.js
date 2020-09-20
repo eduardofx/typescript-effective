0303dd9f6aaddde2c4ec60f1e33b8ea78b63a6aa
eduardo.ferreira <eduardo.ferreira@wisereducacao.com>
2020-09-20 12:13:59 -0300

    feat: use ts in js

---
+++ index.js
@@ -0,0 +1,14 @@
+//@ts-check
+
+/**
+ * @param {string} message
+ * @return {string}
+ */
+function getMessage(message){
+    return message
+}
+
+const message = getMessage('')
+console.log(message);
+
+//exec - $ node index.js
