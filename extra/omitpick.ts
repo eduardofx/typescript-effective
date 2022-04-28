da512df056fc989fe6927fcb8b9cae30ad00becd
eduardo <eduardok.fx@gmail.com>
2022-04-27 23:20:16 -0300

    feat: omit & pick

---
+++ extra/omitpick.ts
@@ -0,0 +1,23 @@
+interface User {
+    id?: number;
+    firstname: string;
+    lastname?: string;
+    age: number;
+    telephone?: number;
+    twitter?: string;
+  }
+
+type UserFullname = Pick<User, 'firstname' | 'lastname'>;
+
+const userName: UserFullname = {
+  firstname: 'Chris',
+  lastname: 'Bongers',
+};
+
+type UserPost = Omit<User, 'id'>;
+
+const updateUser: UserPost = {
+  firstname: 'Chris',
+  lastname: 'Bongers',
+  age: 32,
+};
