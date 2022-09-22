3a9ed0ddde0e5232e02f9db876f4b2c6e18d6c2b
Eduardo <eduardok.fx@gmail.com>
2022-09-22 09:59:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,18 @@
  * @param {string} message
  * @return {string}
  */
-function getMessage(message){
+ function getMessage(message){
     return message
 }
 
 const message = getMessage('Show message')
 console.log(message);
+
+
+/** @type {number} */
+let x;
+ 
+x = 0; // OK
+//x = false; // Not OK
 
 //exec - $ node index.js
